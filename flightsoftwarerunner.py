e0adeae6c98c02534384befa8d6b9debba8e8355
tamara <tamigr.2293@gmail.com>
2021-10-18 22:47:15 -0300

    clean code in flightsoftwarerunner.py

--- flightsoftwarerunner.py
+++ flightsoftwarerunner.py
@@ -3,13 +3,6 @@ from subprocess import Popen, PIPE
 from fuzzcspzmqnode import *
 from proc_info import *
 import time
-import sys
-
-SCH_TRX_PORT_TM = 9               # ///< Telemetry port
-SCH_TRX_PORT_TC = 10               # ///< Telecommands port
-SCH_TRX_PORT_RPT = 11               # ///< Digirepeater port (resend packets)
-SCH_TRX_PORT_CMD = 12               # ///< Commands port (execute console commands)
-SCH_TRX_PORT_DBG = 13               # ///< Debug port, logs output
 
 
 class FlightSoftwareRunner(Runner):
@@ -29,7 +22,7 @@ class FlightSoftwareRunner(Runner):
         # Send commands to the flight software through zmq
         dest = "1"
         addr = "9"
-        port = str(SCH_TRX_PORT_CMD)
+        port = "12"
         #ipc
         #node = FuzzCspZmqNode(addr, hub_ip="/tmp/suchaifs", proto="ipc")
         #tcp
@@ -42,14 +35,12 @@ class FlightSoftwareRunner(Runner):
         suchai_process = Popen([self.exec_cmd], stdin=PIPE)
         time.sleep(4)
 
-        # node.wait_init_ready()
-
         # Clean database
         print("node send: drp_ebf 1010")  # For debugging purposes
         header = CspHeader(src_node=int(addr), dst_node=int(dest), dst_port=int(port), src_port=55)
         node.send_message("drp_ebf 1010", header)
 
-        # Start sendind random commands
+        # Start sending random commands
         for i in range(0, len(cmds_list)):
             # time.sleep(0.5)  # Give some time to zmqnode threads (writer and reader)
             cmd = cmds_list[i]
@@ -67,21 +58,9 @@ class FlightSoftwareRunner(Runner):
         node.send_message("obc_reset", hdr)
 
         # Get SUCHAI process return code
-        """ while True:
-            output = suchai_process.stdout.readline()
-            print("out: ", output)
-            print("rc: ", suchai_process.poll())
-            if suchai_process.poll() is not None:
-                break
-            if output:
-                print(output)
-            return_code = suchai_process.poll()
-        end_time = time.time()  # End measuring execution time of the sequence
-        print("Return code:", return_code)
-        """
         return_code = suchai_process.wait()
         end_time = time.time()  # End measuring execution time of the sequence
-        print("Return code: ", return_code)  # For debugging purposes"""
+        print("Return code: ", return_code)  # For debugging purposes
 
         # Get commands, results, execution time and memory usage
         executed_cmds = node.filter_cmds_names()
